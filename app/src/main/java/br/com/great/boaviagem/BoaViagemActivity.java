c77ca139cf333267f1172a61121b5f85d6bf8e00
glailton <glailton.rc@gmail.com>
2015-04-06 18:00:29 -0300

    Update da Tela de Login

--- app/src/main/java/br/com/great/boaviagem/BoaViagemActivity.java
+++ app/src/main/java/br/com/great/boaviagem/BoaViagemActivity.java
@@ -2,16 +2,43 @@ package br.com.great.boaviagem;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
 
 
-public class MainActivity extends Activity {
+public class BoaViagemActivity extends Activity {
+
+    private EditText edUser;
+    private EditText edPass;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.login);
+
+        edUser = (EditText) findViewById(R.id.ed_user);
+        edPass = (EditText) findViewById(R.id.ed_pass);
+    }
+
+    public void loginApp(View v){
+        String user = edUser.getText().toString();
+        String pass = edPass.getText().toString();
+        Log.d("Tag Name", user);
+        Log.d("Tag Name", pass);
+
+
+        if("great".equals(user) && "lge123".equals(pass)){
+            Toast toast = Toast.makeText(getApplicationContext(), "Usuário e Senha corretos!",Toast.LENGTH_LONG);
+            toast.show();
+        } else{
+            String msgError = getString(R.string.auth_fail);
+            Toast toast = Toast.makeText(getApplicationContext(), msgError ,Toast.LENGTH_LONG);
+            toast.show();
+        }
     }
 
 
