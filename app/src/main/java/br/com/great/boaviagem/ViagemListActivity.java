ad7be23257fbbc86cff6f342db7e36370f649d4c
glailton <glailton.rc@gmail.com>
2015-04-14 17:42:09 -0300

    Adicionando ProgressBar

--- app/src/main/java/br/com/great/boaviagem/ViagemListActivity.java
+++ app/src/main/java/br/com/great/boaviagem/ViagemListActivity.java
@@ -5,6 +5,7 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.AdapterView;
+import android.widget.ProgressBar;
 import android.widget.SimpleAdapter;
 import android.widget.Toast;
 
@@ -14,7 +15,7 @@ import java.util.List;
 import java.util.Map;
 
 
-public class ViagemListActivity extends ListActivity implements AdapterView.OnItemClickListener {
+public class ViagemListActivity extends ListActivity implements AdapterView.OnItemClickListener, SimpleAdapter.ViewBinder {
 
     private List<Map<String, Object>> viagens;
 
@@ -27,11 +28,13 @@ public class ViagemListActivity extends ListActivity implements AdapterView.OnIt
         ListView listView = getListView();
         listView.setOnItemClickListener(this);*/
 
-        String[] de = {"imagem", "destino", "data", "total"};
-        int[] para = {R.id.tipoViagem, R.id.destino, R.id.data, R.id.valor};
+        String[] de = {"imagem", "destino", "data", "total", "barraProgresso"};
+        int[] para = {R.id.tipoViagem, R.id.destino, R.id.data, R.id.valor, R.id.barraProgresso};
 
         SimpleAdapter adapter = new SimpleAdapter(this, listarViagens(),
                 R.layout.lista_viagem, de, para);
+
+        adapter.setViewBinder(this);
 
         setListAdapter(adapter);
         getListView().setOnItemClickListener(this);
@@ -45,6 +48,7 @@ public class ViagemListActivity extends ListActivity implements AdapterView.OnIt
         item.put("destino", "São Paulo");
         item.put("data","02/02/2012 a 04/02/2012");
         item.put("total","Gasto total R$ 314,98");
+        item.put("barraProgresso", new Double[]{500.0, 450.0, 314.98});
         viagens.add(item);
 
         item = new HashMap<String,Object>();
@@ -52,6 +56,7 @@ public class ViagemListActivity extends ListActivity implements AdapterView.OnIt
         item.put("destino","Maceió");
         item.put("data","14/05/2012 a 22/05/2012");
         item.put("total","Gasto total R$ 25834,67");
+        item.put("barraProgresso", new Double[]{500.0, 450.0, 314.98});
         viagens.add(item);
 
         return viagens;
@@ -70,5 +75,17 @@ public class ViagemListActivity extends ListActivity implements AdapterView.OnIt
         String mensagem = "Viagem selecionada: " + textView.getText();
         Toast.makeText(getApplicationContext(), mensagem, Toast.LENGTH_LONG).show();
         startActivity(new Intent(this, GastoListActivity.class));*/
+    }
+
+    public boolean setViewValue(View view, Object data, String textRepresentation){
+        if (view.getId() == R.id.barraProgresso){
+            Double valores[] = (Double[]) data;
+            ProgressBar progressBar = (ProgressBar) view;
+            progressBar.setMax(valores[0].intValue());
+            progressBar.setSecondaryProgress(valores[1].intValue());
+            progressBar.setProgress(valores[2].intValue());
+            return true;
+        }
+        return false;
     }
 }
