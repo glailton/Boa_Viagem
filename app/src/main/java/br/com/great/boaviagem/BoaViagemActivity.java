64920c208128fcbed9fa48634fa40380a9bceb1e
glailton <glailton.rc@gmail.com>
2015-04-15 12:41:27 -0300

    Adicionando Preferences

--- app/src/main/java/br/com/great/boaviagem/BoaViagemActivity.java
+++ app/src/main/java/br/com/great/boaviagem/BoaViagemActivity.java
@@ -2,19 +2,23 @@ package br.com.great.boaviagem;
 
 import android.app.Activity;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.CheckBox;
 import android.widget.EditText;
 import android.widget.Toast;
 
 
 public class BoaViagemActivity extends Activity {
 
+    private static final String MANTER_CONECTADO = "manter_conectado";
     private EditText edUser;
     private EditText edPass;
+    private CheckBox manterConectado;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -23,6 +27,15 @@ public class BoaViagemActivity extends Activity {
 
         edUser = (EditText) findViewById(R.id.ed_user);
         edPass = (EditText) findViewById(R.id.ed_pass);
+        manterConectado = (CheckBox) findViewById(R.id.manterConectado);
+
+        SharedPreferences preferencias = getPreferences(MODE_PRIVATE);
+        boolean conectado = preferencias.getBoolean(MANTER_CONECTADO, false);
+
+        if (conectado) {
+            startActivity(new Intent(this, DashboardActivity.class));
+        }
+
     }
 
     public void loginApp(View v){
@@ -32,10 +45,22 @@ public class BoaViagemActivity extends Activity {
         Log.d("Tag Name", pass);
 
 
-   //     if("great".equals(user) && "lge".equals(pass)){
+        if("great".equals(user) && "lge".equals(pass)){
+
+            SharedPreferences preferencias = getPreferences(MODE_PRIVATE);
+
+            SharedPreferences.Editor editor = preferencias.edit();
+            editor.putBoolean(MANTER_CONECTADO, manterConectado.isChecked());
+            editor.commit();
+            startActivity(new Intent(this,DashboardActivity.class));
+        }else {
+            String mensagemErro = getString(R.string.auth_fail);
+            Toast toast = Toast.makeText(getApplicationContext(), mensagemErro ,Toast.LENGTH_LONG);
+            toast.show();
+        }
           /*  Toast toast = Toast.makeText(getApplicationContext(), "Usuário e Senha corretos!",Toast.LENGTH_LONG);
             toast.show();*/
-            startActivity(new Intent(this,DashboardActivity.class));
+       //     startActivity(new Intent(this,DashboardActivity.class));
     //    } else{
       //      String msgError = getString(R.string.auth_fail);
         //    Toast toast = Toast.makeText(getApplicationContext(), msgError ,Toast.LENGTH_LONG);
---
+++ app/src/main/java/br/com/great/boaviagem/ConfiguracoesActivity.java
@@ -0,0 +1,14 @@
+package br.com.great.boaviagem;
+
+import android.os.Bundle;
+import android.preference.PreferenceActivity;
+
+
+public class ConfiguracoesActivity extends PreferenceActivity {
+    @SuppressWarnings("deprecation")
+    @Override
+    protected void onCreate (Bundle savedInstanceState){
+        super.onCreate(savedInstanceState);
+        addPreferencesFromResource(R.xml.preference);
+    }
+}
